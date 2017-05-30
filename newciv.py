10890b2b614fb6d854621212f2b3e6720930593c
James Wilson <oh.that.wilson@googlemail.com>
2017-05-30 22:32:56 +0100

    now stores terrain types properly

--- newciv.py
+++ newciv.py
@@ -29,8 +29,8 @@ class Terrain(CivModel):
 
 db.create_tables([VetLevel,Unit,Terrain],safe=True)
 assignment = re.compile(r'(?P<name>\w+)\s*=\s*(?P<data>.*)')
+
 #UNITS
-#TODO Need to do unit classes first
 
 with open('units.ruleset') as unitsfile:
     unitdef = re.compile(r'\[unit_(?P<unitname>\w+)')
@@ -61,6 +61,28 @@ with open('units.ruleset') as unitsfile:
 
             new_unit.save()
 
+#TERRAIN
+with open('terrain.ruleset') as terrainfile:
+    terrainblock = re.compile(r'\[terrain_(?P<terrainname>\w+)\]')
+    for line in terrainfile:
+        match = terrainblock.match(line)
+        if match:
+            new_terrain = Terrain()
+            new_terrain.name = match.group("terrainname").replace("_"," ").title()
 
+            for stat in takewhile(lambda x: x!= "\n", terrainfile):
+                capture = assignment.match(stat)
+                if capture:
+                    name = capture.group("name")
+                    data = capture.group("data")
+                    try:
+                        data = int(data)
+                    except ValueError:
+                        pass
+                    if name == "class":
+                        new_terrain.terrain_class = data[1:-1]
+                    if name == "defense_bonus":
+                        new_terrain.defence_bonus = data
+            new_terrain.save()
 
 
